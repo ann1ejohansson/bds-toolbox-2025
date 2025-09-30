04d0a7de3b32f30d7fe7d0ffd105984c1115854b
ann1ejohansson <162436676+ann1ejohansson@users.noreply.github.com>
2025-09-30 14:12:05 +0200

    Update allocate-groups.r

--- scripts/allocate-groups.r
+++ scripts/allocate-groups.r
@@ -1,10 +1,12 @@
 # ---------------------------
 # Function: allocate_groups
-# Rules:
-# - Split by RQ into groups of 3
-# - Leftover 1 or 2 -> merged into earlier groups (3–4 only)
-# - Special case: exactly 5 students -> split into 2 + 3
+# Rules: 
+# - Groups are only size 2 or 3 
+# - Split by RQ into groups of 3 
+# - Special case: exactly 5 -> 2 + 3 
+# - Special case: exactly 4 -> 2 + 2
 # ---------------------------
+
 allocate_groups <- function(data) {
   set.seed(NULL)  # randomize
   
@@ -21,52 +23,56 @@ allocate_groups <- function(data) {
     students <- students[sample(nrow(students)), ]
     n <- nrow(students)
     
-    # --- Exactly 5 students ---
-    if (n == 5) {
-      groups[[group_id]] <- students[1:2, ]   # group of 2
+    # --- Handle small cases ---
+    if (n == 1) {
+      groups[[group_id]] <- students
       group_id <- group_id + 1
-      groups[[group_id]] <- students[3:5, ]   # group of 3
+      next
+    }
+    if (n == 2) {
+      groups[[group_id]] <- students
+      group_id <- group_id + 1
+      next
+    }
+    if (n == 4) {
+      groups[[group_id]] <- students[1:2, ]
+      group_id <- group_id + 1
+      groups[[group_id]] <- students[3:4, ]
+      group_id <- group_id + 1
+      next
+    }
+    if (n == 5) {
+      groups[[group_id]] <- students[1:2, ]
+      group_id <- group_id + 1
+      groups[[group_id]] <- students[3:5, ]
       group_id <- group_id + 1
       next
     }
     
-    # --- Less than 3 students ---
-    if (n < 3) {
-      groups[[group_id]] <- students
+    # --- Normal case ---
+    full_groups <- n %/% 3   # how many full groups of 3
+    leftover <- n %% 3       # 0, 1, or 2
+    
+    # Make groups of 3
+    if (full_groups > 0) {
+      for (i in 1:full_groups) {
+        idx <- ((i - 1) * 3 + 1):(i * 3)
+        groups[[group_id]] <- students[idx, ]
+        group_id <- group_id + 1
+      }
+    }
+    
+    # Handle leftover (1 or 2)
+    if (leftover == 1) {
+      # Turn last 3 into a 2 + 2 split
+      last_group <- groups[[group_id - 1]]
+      groups[[group_id - 1]] <- last_group[1:2, ]
+      groups[[group_id]] <- rbind(last_group[3, ], students[n, ])
       group_id <- group_id + 1
-    } else {
-      # --- Normal case ---
-      full_groups <- n %/% 3   # how many full groups of 3
-      leftover <- n %% 3       # 0, 1, or 2
-      
-      # Make full groups of 3
-      if (full_groups > 0) {
-        for (i in 1:full_groups) {
-          idx <- ((i - 1) * 3 + 1):(i * 3)
-          groups[[group_id]] <- students[idx, ]
-          group_id <- group_id + 1
-        }
-      }
-      
-      # Handle leftover (1 or 2 students)
-      if (leftover > 0) {
-        idx <- (n - leftover + 1):n
-        leftover_students <- students[idx, ]
-        
-        if (leftover == 1) {
-          groups[[group_id - 1]] <- rbind(groups[[group_id - 1]], leftover_students)
-        } else if (leftover == 2) {
-          groups[[group_id - 1]] <- rbind(groups[[group_id - 1]], leftover_students[1, ])
-          
-          if (full_groups > 1) {
-            groups[[group_id - 2]] <- rbind(groups[[group_id - 2]], leftover_students[2, ])
-          } else {
-            groups[[group_id - 1]] <- rbind(groups[[group_id - 1]], leftover_students[2, ])
-          }
-        }
-      }
-      
-      
+    } else if (leftover == 2) {
+      idx <- (n - 1):n
+      groups[[group_id]] <- students[idx, ]
+      group_id <- group_id + 1
     }
   }
   
@@ -86,9 +92,10 @@ allocate_groups <- function(data) {
 # 
 # # test with n < 3
 # students[which(students$RQ == "RQ1"), "RQ"] <- "RQ2"
-# students[1:2, "RQ"] <- "RQ1" 
+# students[1:2, "RQ"] <- "RQ1"
 
 students <- read.csv("scripts/test.csv", header = TRUE)
+
 # ---------------------------
 # Allocate groups 
 # ---------------------------
